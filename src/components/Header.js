c0cb02a07ffaa499d100f3de5154dda09fd27b89
DeveloperRaj <rajchhatrala2001@gmail.com>
2019-10-31 16:29:48 +0530

    header added

---
+++ src/components/Header.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './css/header.css';
+
+function Header(props) {
+
+	let isOpen = false;
+
+	const toggleNav = (e) => {
+		let nav = document.getElementById('nav');
+		if (!isOpen) {
+			nav.style.display = "block";
+			isOpen = true;
+		} else {
+			nav.style.display = "none";
+			isOpen = false;
+		}
+	}
+
+	return (
+		<header className="headerContainer">
+			<div className="siteLogo">
+				<u>S</u>t<u>o</u>r<u>y</u>&nbsp;S<u>t</u>a<u>c</u>k
+			</div>
+			<button onClick={toggleNav} type="button" className="btn">
+				Menu
+			</button>
+			<div className="links" id="nav">
+				<ul>
+					<li><Link to="/" className="linkItem">home</Link></li>
+					<li><Link to="/" className="linkItem">topstories</Link></li>
+					<li><Link to="/" className="linkItem">askstories</Link></li>
+					<li><Link to="/" className="linkItem">showstories</Link></li>
+					<li><Link to="/" className="linkItem">jobstories</Link></li>
+					<li><Link to="/" className="linkItem">users</Link></li>
+				</ul>
+			</div>
+		</header>
+	)
+}
+
+export default Header;
